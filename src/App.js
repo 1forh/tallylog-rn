feb31eb73d712c66e4e814b3491fa7ed6dbe579c
1forh <1forhlol@gmail.com>
2021-05-21 22:58:29 -0400

    🎉

--- src/App.js
+++ src/App.js
@@ -6,107 +6,98 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StatusBar, StyleSheet, Text, Pressable, View } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// const Section = ({children, title}): Node => {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   return (
+//     <View style={styles.sectionContainer}>
+//       <Text
+//         style={[
+//           styles.sectionTitle,
+//           {
+//             color: isDarkMode ? Colors.white : Colors.black,
+//           },
+//         ]}>
+//         {title}
+//       </Text>
+//       <Text
+//         style={[
+//           styles.sectionDescription,
+//           {
+//             color: isDarkMode ? Colors.light : Colors.dark,
+//           },
+//         ]}>
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// };
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+export default function App() {
+  // const isDarkMode = useColorScheme() === 'dark';
+  const [tally, setTally] = useState(0);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView>
+      <StatusBar />
+      <View style={styles.wrapper}>
+        <View style={styles.inner}>
+          <Text style={styles.name}>Testing</Text>
+          <Text style={styles.tally}>{tally}</Text>
         </View>
-      </ScrollView>
+        <View style={styles.buttons}>
+          <Pressable style={styles.button} onPress={() => setTally(tally - 1)}>
+            <Text style={styles.buttonText}>-</Text>
+          </Pressable>
+          <Pressable style={styles.button} onPress={() => setTally(tally + 1)}>
+            <Text style={styles.buttonText}>+</Text>
+          </Pressable>
+        </View>
+      </View>
     </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  inner: {
+    marginTop: 100,
+    flexGrow: 1,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  name: {
+    marginBottom: 20,
+    fontSize: 36,
+    textAlign: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  tally: {
+    fontSize: 72,
+    fontWeight: '800',
+    textAlign: 'center',
+  },
+  buttons: {
+    padding: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    display: 'flex',
+  },
+  button: {
+    backgroundColor: '#000',
+    width: '45%',
+    height: 60,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 3,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 42,
+    lineHeight: 44,
   },
 });
-
-export default App;
