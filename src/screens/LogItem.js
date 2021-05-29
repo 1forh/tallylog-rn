d8c4a69b8702bb0d089c7c73f61d143c211fbe7e
1forh <1forhlol@gmail.com>
2021-05-28 22:19:16 -0400

    🚧

---
+++ src/screens/LogItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function LogItem() {
+  return (
+    <View>
+      <Text>Log Item</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
