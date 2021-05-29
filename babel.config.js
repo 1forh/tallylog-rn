470030bad0923a5cdc84b2a13188ed7e96801026
1forh <1forhlol@gmail.com>
2021-05-28 22:12:02 -0400

    🚧

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
+
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@tailwind': ['./src/utils/tailwind.js'],
+            '@screens': ['./src/screens'],
+            '@components': ['./src/components'],
+            '@utils': ['./src/utils'],
+            '@store': ['./src/store'],
+            '@assets': ['./assets'],
+          },
+        },
+      ],
+    ],
   };
 };
---
+++ src/utils/firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAMZRiB0xDxfJitPLnRQi8mDLnzBzj5oC4',
+  authDomain: 'tally-log--live.firebaseapp.com',
+  databaseURL: 'https://tally-log--live.east4.firebasedatabase.app',
+  projectId: 'tally-log--live',
+  storageBucket: 'tally-log--live.appspot.com',
+  messagingSenderId: 'sender-id',
+  appId: '1:895186639982:ios:29056c18514424b1521a7f',
+  measurementId: 'G-measurement-id',
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app(); // if already initialized, use that one
+}
+
+export { firebase };
